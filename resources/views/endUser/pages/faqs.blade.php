f2e09d7155927eaa69f4fdc3a8bf1e3685678f4e
Abderazek Emad <a.razek@gmail.com>
2023-12-23 17:46:28 +0200

    add links to mobile navlinks & fix arrow diriction

--- resources/views/endUser/pages/faqs.blade.php
+++ resources/views/endUser/pages/faqs.blade.php
@@ -27,11 +27,11 @@
             <ul class="nav nav-pills mb-3 justify-content-center" id="pills-tab" role="tablist">
                 @foreach ($categories as $category)
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link {{ $loop->first ? 'active' : '' }}" id="pills-category-{{ $category->id }}-tab"
-                                data-bs-toggle="pill" data-bs-target="#pills-category-{{ $category->id }}"
-                                type="button" role="tab" aria-controls="pills-category-{{ $category->id }}"
-                                aria-selected="{{ $loop->first ? 'true' : 'false' }}"
-                                data-category-id="{{ $category->id }}">
+                        <button class="nav-link {{ $loop->first ? 'active' : '' }}"
+                            id="pills-category-{{ $category->id }}-tab" data-bs-toggle="pill"
+                            data-bs-target="#pills-category-{{ $category->id }}" type="button" role="tab"
+                            aria-controls="pills-category-{{ $category->id }}"
+                            aria-selected="{{ $loop->first ? 'true' : 'false' }}" data-category-id="{{ $category->id }}">
                             {{ $category->category }}
                         </button>
                     </li>
@@ -39,32 +39,34 @@
             </ul>
             <div class="tab-content" id="pills-tabContent">
                 @foreach ($categories as $category)
-                <div class="tab-pane fade {{ $loop->first ? 'active show' : '' }}" id="pills-category-{{ $category->id }}" role="tabpanel" aria-labelledby="pills-category-{{ $category->id }}-tab">
+                    <div class="tab-pane fade {{ $loop->first ? 'active show' : '' }}"
+                        id="pills-category-{{ $category->id }}" role="tabpanel"
+                        aria-labelledby="pills-category-{{ $category->id }}-tab">
 
-                </div>
+                    </div>
                 @endforeach
-{{--                <div class="tab-pane fade show active" id="pills-carigory-1" role="tabpanel"--}}
-{{--                    aria-labelledby="pills-carigory-1-tab" tabindex="0">--}}
-{{--                    <div id="accordionExample" class="accordion h-100">--}}
-{{--                        <div class="accordion-item">--}}
-{{--                                <h3 id="headingOne" class="accordion-header">--}}
-{{--                                    <button type="button" class="accordion-button" data-bs-toggle="collapse"--}}
-{{--                                            data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">--}}
-{{--                                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum, sed!--}}
-{{--                                    </button>--}}
-{{--                                </h3>--}}
-{{--                                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"--}}
-{{--                                     data-bs-parent="#accordionExample">--}}
-{{--                                    <div class="accordion-body">--}}
-{{--                                        <p>--}}
-{{--                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum odit similique--}}
-{{--                                            cupiditate cum accusamus blanditiis beatae natus ipsam, facilis,--}}
-{{--                                        </p>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
+                {{--                <div class="tab-pane fade show active" id="pills-carigory-1" role="tabpanel" --}}
+                {{--                    aria-labelledby="pills-carigory-1-tab" tabindex="0"> --}}
+                {{--                    <div id="accordionExample" class="accordion h-100"> --}}
+                {{--                        <div class="accordion-item"> --}}
+                {{--                                <h3 id="headingOne" class="accordion-header"> --}}
+                {{--                                    <button type="button" class="accordion-button" data-bs-toggle="collapse" --}}
+                {{--                                            data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> --}}
+                {{--                                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum, sed! --}}
+                {{--                                    </button> --}}
+                {{--                                </h3> --}}
+                {{--                                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" --}}
+                {{--                                     data-bs-parent="#accordionExample"> --}}
+                {{--                                    <div class="accordion-body"> --}}
+                {{--                                        <p> --}}
+                {{--                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum odit similique --}}
+                {{--                                            cupiditate cum accusamus blanditiis beatae natus ipsam, facilis, --}}
+                {{--                                        </p> --}}
+                {{--                                    </div> --}}
+                {{--                                </div> --}}
+                {{--                            </div> --}}
+                {{--                    </div> --}}
+                {{--                </div> --}}
             </div>
         </div>
     </section>
@@ -74,7 +76,7 @@
 
 @push('js')
     <script !src="">
-        document.addEventListener('DOMContentLoaded', function () {
+        document.addEventListener('DOMContentLoaded', function() {
             const tabs = document.querySelectorAll('.nav-link[data-category-id]');
 
             function loadCategoryContent(categoryId) {
@@ -87,7 +89,7 @@
                             const faqContent = data.map(faq =>
                                 `<div class="accordion-item">
                             <h3 class="accordion-header">
-                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-${faq.id}">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-${faq.id}">
                                     ${faq.question}
                                 </button>
                             </h3>
@@ -105,7 +107,7 @@
             }
 
             tabs.forEach(tab => {
-                tab.addEventListener('click', function () {
+                tab.addEventListener('click', function() {
                     const categoryId = this.getAttribute('data-category-id');
                     loadCategoryContent(categoryId);
                 });
@@ -117,6 +119,5 @@
                 loadCategoryContent(firstTabCategoryId);
             }
         });
-
     </script>
 @endpush
