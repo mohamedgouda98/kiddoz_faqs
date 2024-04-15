a9f73e0592238c97a0333a118f5d5a402e1bfd0a
Genuis-code <moh.aly8890@gmail.com>
2024-04-15 16:34:34 +0200

    update_article

---
+++ app/Console/Commands/AdminSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'run:admin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $users = [
+            [
+                'id'             => 1,
+                'name'           => 'Admin',
+                'email'          => 'admin@mail.com',
+                'password'       => Hash::make('12345678'),
+                'remember_token' => null,
+            ],
+        ];
+
+        User::insert($users);
+        return 0;
+    }
+}
